f3d7ec57b27f5b41bbfe516744452eec7de55f7b
Michael Hearn <git@mikehearn.net>
2017-07-09 22:26:31 -0400

    Add page titles for each section

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,7 +5,7 @@ export default Ember.Route.extend({
     return this._super();
   },
   title: function(tokens) {
-    let title = "Ashley Mas & Michael Hearn's Wedding";
+    let title = "Ashley Mas & Michael Hearn's Wedding Website";
     if (tokens.length > 0) {
       title = `${tokens.join(" - ")} - ${title}`;
     }
