f7d89e0c48464b37029de462b61b849e88022d1e
Michael Hearn <git@mikehearn.net>
2017-07-25 20:02:18 -0400

    Update basic tests to pass

--- tests/unit/routes/rsvp-test.js
+++ tests/unit/routes/rsvp-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:rsvp', 'Unit | Route | rsvp', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['service:headData']
 });
 
 test('it exists', function(assert) {
--- tests/unit/routes/where-to-stay-test.js
+++ tests/unit/routes/where-to-stay-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:where-to-stay', 'Unit | Route | where to stay', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['service:headData']
 });
 
 test('it exists', function(assert) {
