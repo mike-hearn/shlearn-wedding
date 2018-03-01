0d79d6c0b7c2a612b7211be08a500aa977e52bd3
Michael Hearn <git@mikehearn.net>
2018-02-28 23:07:36 -0500

    Minor changes to rsvp tests

--- tests/acceptance/rsvp-test.js
+++ tests/acceptance/rsvp-test.js
@@ -1,3 +1,4 @@
+import $ from "jquery";
 import { module, test } from "qunit";
 import { visit, fillIn, click } from "@ember/test-helpers";
 import { setupApplicationTest } from "ember-qunit";
@@ -5,27 +6,35 @@ import { setupApplicationTest } from "ember-qunit";
 module("Acceptance | rsvp", function(hooks) {
   setupApplicationTest(hooks);
 
-  test("one match, only invitation is shown", async function(assert) {
+  test("person match: with one match, only invitation is shown", async function(assert) {
     let { fullName } = await server.create("person");
 
     await visit("/rsvp");
     await fillIn("#person-search input", "John Doe");
     await click("#person-search button");
 
-    let personHeader = document.querySelector('.rsvp--person-name').textContent;
+    let personHeader = document.querySelector(".rsvp--person-name").textContent;
     assert.equal(personHeader, fullName);
 
     server.db.emptyData();
   });
 
-  test("two matches, show name picker", async function(assert) {
-    await server.createList("person", 2);
+  test("person match: with two matches, show name picker", async function(assert) {
+    let people = await server.createList("person", 2);
 
     await visit("/rsvp");
     await fillIn("#person-search input", "John Doe");
     await click("#person-search button");
 
-    assert.equal(document.querySelectorAll('.rsvp--person-option').length, 2);
+    await assert.equal($(".rsvp__multiple-matches .person-option").length, 2);
+
+    await $(".rsvp__multiple-matches .person-option")
+      .first()
+      .click();
+    await $(".rsvp__multiple-matches button").click();
+
+    let personHeader = document.querySelector(".rsvp--person-name").textContent;
+    assert.equal(personHeader, people[0].fullName);
 
     server.db.emptyData();
   });
