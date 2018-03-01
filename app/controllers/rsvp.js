48b2463b71b30f08314295fbae777cf460038ef0
Michael Hearn <git@mikehearn.net>
2018-02-28 20:18:13 -0500

    Update yarn.lock & minor eslint import fix

--- app/controllers/rsvp.js
+++ app/controllers/rsvp.js
@@ -1,7 +1,7 @@
-import Ember from 'ember';
+import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 
-export default Ember.Controller.extend({
+export default Controller.extend({
   singleMatch: computed.equal('peopleMatches', 1),
   person: true
 });
