3c93db717b5a99d73ff63ee751459b983866b156
Michael Hearn <git@mikehearn.net>
2018-03-11 16:36:18 -0400

    Passing integration test for two known guests

--- mirage/factories/invitation.js
+++ mirage/factories/invitation.js
@@ -2,6 +2,7 @@ import { Factory, trait } from 'ember-cli-mirage';
 
 export default Factory.extend({
   musicSuggestions() { return ""; },
+
   withTwoGuests: trait({
     afterCreate(invitation, server) {
       let guests = server.createList('person', 2, { invitation });
--- tests/integration/components/rsvp/full-rsvp-form-test.js
+++ tests/integration/components/rsvp/full-rsvp-form-test.js
@@ -3,20 +3,27 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import Object from '@ember/object';
 
 module('Integration | Component | rsvp/full-rsvp-form', function(hooks) {
   setupRenderingTest(hooks);
 
   test('shows two RSVPs for two known persons', async function(assert) {
-    let invitation = await server.create('invitation', 'withTwoGuests');
+    let people = server.createList('person', 2).map(p => Object.create(p.attrs));
+    let invitation = Object.create({
+      guests: people
+    })
 
-    await this.set('invitation', invitation);
+    this.set('invitation', invitation);
 
     await render(hbs`{{rsvp/full-rsvp-form invitation=invitation}}`);
 
-    assert.equal(this.element.textContent, 'fail');
+    assert.ok($(`.rsvp__individual-rsvp:contains(${people[0].fullName})`).length > 0);
+    assert.ok($(`.rsvp__individual-rsvp:contains(${people[1].fullName})`).length > 0);
+    assert.ok($(`.rsvp__individual-rsvp`).length === 2);
 
     server.db.emptyData();
 
   });
+
 });
