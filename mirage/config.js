fc3592461a468325fce06ab05c93d00867ff3f1b
Michael Hearn <git@mikehearn.net>
2018-03-23 22:56:28 -0400

    Update mirage tests to be fancy

--- mirage/config.js
+++ mirage/config.js
@@ -1,30 +1,18 @@
 import config from '../config/environment';
 
 export default function() {
+  this.urlPrefix = config.apiURL; // make this `http://localhost:8080`, for example, if your API is on a different server
+  this.timing = 1000; // delay for each request, automatically set to 0 during testing
 
-  // These comments are here to help you get started. Feel free to delete them.
+  /* If the `passthrough` key is found in localStorage, ignore the entire
+   * Mirage mock configuration and passthrough everything. The `return` assures
+   * that we don't process the rest of the config.*/
+  if (window.localStorage.passthrough) {
+    this.passthrough('**');
+    return;
+  }
 
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
-
-  this.urlPrefix = config.apiURL;    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
-  this.timing = 1000;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    http://www.ember-cli-mirage.com/docs/v0.3.x/shorthands/
-  */
+  /* Mirage mocked endpoints */
   this.get('/people/', function(schema) {
     return schema.people.where({isUnknownGuest: false});
   });
--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,8 +1,24 @@
 export default function(server) {
-  /*
-    Seed your development database using your factories.
-    This data will not be loaded in your tests.
-  */
-
-    server.create('invitation', 'withTwoGuestsOneUnknown');
+  /* We can change the scenario that Mirage generates by applying different
+   * values to window.localStorage.testScenario. In automation tests outside of
+   * the Ember/Mirage context, this lets us mock various scenarios without
+   * having direct access to the server.create method.
+   *
+   * If you (i.e. me) get stuck on a scenario for any reason, clear the
+   * localStorage key from the JS console with:
+   * window.localStorage.removeItem('testScenario')
+   */
+  switch (window.localStorage.testScenario) {
+    case 'withTwoGuestsOneUnknown':
+      server.create('invitation', 'withTwoGuestsOneUnknown');
+      break;
+    case 'withOneGuest':
+      server.create('invitation', 'withOneGuest');
+      break;
+    case 'withTwoGuests':
+      server.create('invitation', 'withTwoGuests');
+      break;
+    default:
+      server.create('invitation', 'withTwoGuestsOneUnknown');
+  }
 }
