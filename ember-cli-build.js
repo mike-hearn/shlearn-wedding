be746465f2bec14d7724de5ce4386a4828c28c60
Michael Hearn <git@mikehearn.net>
2018-03-26 22:47:31 -0400

    Fix bad fingerprinting in photos section

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,6 +8,9 @@ module.exports = function(defaults) {
       'bootstrapVersion': 4,
       'importBootstrapFont': false,
       'importBootstrapCSS': false
+    },
+    fingerprint: {
+      exclude: ['website_photos']
     }
   });
 
